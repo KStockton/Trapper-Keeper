6c7392fc71eb887dc8a2c2f2b1828d2b4cee1a16
Michael KS <kingstockton@gmail.com>
2019-05-20 14:58:48 -0600

    add scss setup
    Co-authored-by Matthew Kaplan <kaplan.matthew.p@gmail.com>
    Co-authored-by: Rachael Drennan <rdrennan0@gmail.com>

---
+++ src/Containers/Main/Main.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Main extends Component {
+  render() {
+    return (
+      <div className="Main-wrapper">
+        <h1>&hearts; Hello</h1>
+      </div>
+    )
+  }
+}
