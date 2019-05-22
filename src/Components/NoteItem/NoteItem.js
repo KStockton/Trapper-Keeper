5a96e958def05d6cd8c22e9ae535ac40b5c8c970
Michael KS <kingstockton@gmail.com>
2019-05-22 13:16:37 -0600

    made small changes to checkbox

--- src/Components/NoteItem/NoteItem.js
+++ src/Components/NoteItem/NoteItem.js
@@ -2,12 +2,26 @@ import React, { Component } from "react";
 // import "./NoteItem.scss";
 
 class NoteItem extends Component {
+  constructor(){
+    super()
+    this.state = {
+      listItem: ''
+    }
+  }
+
+  handleChange = (event) => {
+    console.log(event)
+    const { name, value } = event.target
+    this.setState({[name] : value})
+  }
+
   render() {
+    const {listItem} = this.state
     return (
       <div className="note-item-component">
         <input type="checkbox" />
-        <input type="text" placeholder="List Item" className="list-item"/>
-        <button className="remove-content">x</button>
+        <input name="listItem" type="text" placeholder="List Item" className="list-item" value={this.state.listItem} onChange={this.handleChange}/>
+        <button className={listItem !== '' ? 'no-content' : 'remove-content'} >x</button>
       </div>
     );
   }
