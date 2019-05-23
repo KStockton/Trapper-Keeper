d28b21eebcc9703535fbccc7859f1a5bd0681ea5
Michael KS <kingstockton@gmail.com>
2019-05-23 01:14:35 -0600

    add router and small redux set up index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {render} from 'react-dom';
 import './Sass/index.scss';
 import App from './Components/App/App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux'
+import { Router } from 'react-router'
+import { createStore } from 'redux';
+import { composeWithDevTools } from "redux-devtools-extension";
+import { rootReducer } from './Reducers/index'
 
 
+const store = createStore( rootReducer, composeWithDevTools())
+
+const router = (
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>
+)
 
 
-
-ReactDOM.render(<App />, document.getElementById('root'));
+render(router , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
