2076d039abdc1794db1b48ed12ff8069e892a46b
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-05-24 11:49:16 -0600

    404

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,23 +3,20 @@ import Header from "../Header/Header";
 import NoteContainer from "../NoteContainer/NoteContainer";
 import NotFound from "../NotFound/NotFound";
 import { Route, Switch } from "react-router-dom";
-import { fetchAllNotes } from "../../Api/fetch/fetchAllNotes";
-import NewCard from '../NewCard/NewCard'
-import NoteCard from "../NoteCard/NoteCard";
-
+import NewCard from "../NewCard/NewCard";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="background">
-          <Header />
-          <Switch>
-            <Route exact path to="/" component={NoteContainer}/>
-            <Route exact path="/notes/:id"  />
-            <Route exact path="/new-note" component={NewCard}/>
-          </Switch>
-          <NewCard/>
+        <Header />
+        <Switch>
+          <Route exact path to="/" component={NoteContainer} />
+          <Route exact path="/notes/:id" />
+          <Route exact path="/new-note" component={NewCard} />
+          <Route path="" component={NotFound} />
+        </Switch>
+        <NewCard />
       </div>
     );
   }
