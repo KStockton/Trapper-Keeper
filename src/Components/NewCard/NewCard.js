e6eb8aec567bdd17915eaa21e1359396f8e85ef3
Michael KS <kingstockton@gmail.com>
2019-05-23 21:54:50 -0600

    added material icons for inputs

--- src/Components/NewCard/NewCard.js
+++ src/Components/NewCard/NewCard.js
@@ -19,24 +19,40 @@ export default class NewCard extends Component {
   
 handleKeyPress = () => {
   const {title, listItem, notes} = this.state
+
   const newNote = {id: Date.now(), title, text: listItem, completed: false}
+
   this.setState({notes: [...notes, newNote],
     listItem: ''})
 }
 
   render() {
-    const {title, listItem, notes} = this.state
+    const { notes} = this.state
     console.log(notes)
+    let todos = notes.filter(note => note.completed === false).map(incomplete => {
+      return <section key={incomplete.id}>
+              <i class="material-icons" onClick={this.handleComplete}>check_box_outline_blank</i>
+              <input className="material-icon" value={incomplete.text}/>
+              <i class="material-icons">delete_forever</i>
+            </section>
+
+    })
+
+     
+
+    
     return (
       <div>
-        <form >
+        <form className="NewCard-form">
           <input 
+          type="text"
           name='title'
           value={this.state.title}
           placeholder='title'
           onChange={this.handleChange}
           />
           <input
+          type="text"
           name='listItem'
           value={this.state.listItem}
           placeholder='listItem'
@@ -45,6 +61,7 @@ handleKeyPress = () => {
             if(event.key === 'Enter') this.handleKeyPress() }}
           />
         </form>
+        {todos}
       </div>
     )
   }
