b05509f15e07baa55b4322a84d5d20bc8e65edcb
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-05-28 18:52:22 -0600

    Fix 404 page & link to home when logo is pressed

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,7 +1,5 @@
 import React, { Component } from "react";
-import { Link } from 'react-router-dom';
-
-
+import { Link } from "react-router-dom";
 
 class Header extends Component {
   render() {
@@ -9,9 +7,11 @@ class Header extends Component {
       <div className="header-component ">
         <header className="header">
           <div className="push-pin" />
-          <h1 className="title">TrapperKeeper</h1>
+          <Link to={"/"}>
+            <h1 className="title">TrapperKeeper</h1>
+          </Link>
           <Link to={"/new-note"}>
-          <button className="add-note" />
+            <button className="add-note" />
           </Link>
         </header>
       </div>
