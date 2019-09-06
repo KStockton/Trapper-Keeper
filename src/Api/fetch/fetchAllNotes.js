fede407416f8bb9820f3a7216fb1655d1ad8c1f7
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-09-06 17:18:12 -0600

    Refactor fetchAllNotes into try/catch block

--- src/Api/fetch/fetchAllNotes.js
+++ src/Api/fetch/fetchAllNotes.js
@@ -1,12 +1,10 @@
 import { BASE_URL } from "../utilities";
 
 export const fetchAllNotes = async () => {
-  const response = await fetch(`${BASE_URL}/api/v1/notes`)
-  // .then(response => {
-  //   if (!response.ok) {
-  //     throw Error("Failed to get notes");
-  //   } else {
-      return response.json();
-  //   }
-  // });
+  try {
+    const response = await fetch(`${BASE_URL}/notes`);
+    return response.json();
+  } catch (e) {
+    console.log("Failed to fetch notes", e)
+  } 
 };
