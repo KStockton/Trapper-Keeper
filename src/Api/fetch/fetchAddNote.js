b10a874205073942e3d17ac7851bb25eb844ff73
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-09-06 17:17:50 -0600

    Refactor fetchAddNote into try/catch block

--- src/Api/fetch/fetchAddNote.js
+++ src/Api/fetch/fetchAddNote.js
@@ -1,19 +1,18 @@
-import { BASE_URL } from "../utilities";
+import { BASE_URL } from '../utilities';
 
-export const fetchAddNote = async (title, list) => {
-  const url = `${BASE_URL}/api/v1/notes`;
-  const body = { id: new Date(), title, list };
-  const options = {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(body)
-  };
-  const response = await fetch(url, options)
-  // .then(response => {
-  //   if (!response.ok) {
-  //     throw Error("Note must include Title and Tasks");
-  //   } else {
-      return response.json();
-  //   }
-  // });
+export const fetchAddNote = async (title, notes) => {
+	const url = `${BASE_URL}/notes`;
+	const body = { title, notes };
+	const options = {
+		method: 'POST',
+		headers: { 'Content-Type': 'application/json' },
+		body: JSON.stringify(body)
+	};
+
+	try {
+		const response = await fetch(url, options);
+		return response.json();
+	} catch (e) {
+		console.log('Unable to Add a new note', e);
+	}
 };
