0b066d66b70ed0694178a643efcdf2f7531361f3
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-09-06 17:18:35 -0600

    Refactor fetchDeleteNote into try/catch block

--- src/Api/fetch/fetchDeleteNote.js
+++ src/Api/fetch/fetchDeleteNote.js
@@ -1,14 +1,14 @@
-import { BASE_URL } from "../utilities";
+import { BASE_URL } from '../utilities';
 
 export const fetchDeleteNote = async id => {
-  const url = `${BASE_URL}/api/v1/notes/${id}`;
-  const options = {
-    method: "DELETE"
-  };
+	const url = `${BASE_URL}/notes/${id}`;
+	const options = {
+		method: 'DELETE'
+	};
 
-  const response = await fetch(url, options);
-  if (!response.ok) {
-    throw Error("Failed to delete note");
-  }
-  // return response.json()
+	try {
+		await fetch(url, options);
+	} catch (e) {
+		console.log('Failed to delete note', e);
+	}
 };
